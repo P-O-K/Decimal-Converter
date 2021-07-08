d6b49a730e3c1154a1e10b4577896099a4cf52e7
Patrick O'Keefe <67643795+P-O-K@users.noreply.github.com>
2021-07-08 14:06:58 +0100

    Update decimal_converter.py

--- decimal_converter.py
+++ decimal_converter.py
@@ -4,9 +4,9 @@ from string import digits as BDS
 def DecimalConverter( x, base=10 ):
 	if ( base<2 )|( base>36 ): raise Exception( "Base value must be >= 2 and <= 36" )
 
-	HEX_2_BIN = { key:value for value,key in enumerate( ( BDS+ALS )[ :base ] ) }
+	HEX_2_BIN = { key:value for value, key in enumerate( ( BDS+ALS )[ :base ] ) }
 	TOTAL = 0
-	for index,value in enumerate( reversed( str( x ) ) ):
+	for index, value in enumerate( reversed( str( x ) ) ):
 		try: TOTAL += HEX_2_BIN[ value ] *pow( base, index )
 		except KeyError as sadFace:
 			return False
