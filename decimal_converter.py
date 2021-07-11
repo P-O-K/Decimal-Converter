327ab75444ccdd5f73ed7b9cbeda78a9229a4990
Patrick O'Keefe <67643795+P-O-K@users.noreply.github.com>
2021-07-11 12:26:15 +0100

    Update decimal_converter.py

--- decimal_converter.py
+++ decimal_converter.py
@@ -2,9 +2,7 @@ from string import ascii_lowercase as ALS
 from string import digits as BDS
 
 def DecimalConverter( x, base=10 ):
-	if ( base<2 )|( base>36 ): raise Exception( "Base value must be >= 2 and <= 36" )
-	
-	if( type( x ) == float ): return round( x );
+	if ( base <2 )|( base >36 ): raise Exception( "Base value must be >= 2 and <= 36" )
 
 	HEX_2_BIN = { key:value for value, key in enumerate( ( BDS+ALS )[ :base ] ) }
 	TOTAL = 0
